1c9911edfa33e1c2fc962bd72c611aa3d5e13bf9
willie-gif <williamnyagah2@gmail.com>
2025-07-30 11:44:16 +0300

    Update bikeapp.py

--- bikeapp.py
+++ bikeapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-#from fpdf import FPDF 
+from fpdf import FPDF 
 import io
 from datetime import datetime, timedelta
 from streamlit_option_menu import option_menu 
